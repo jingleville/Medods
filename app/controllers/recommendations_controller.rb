43bf5dd17715e2c69e6f41acb6922ce62326e9a3
jingleville <andrewgavrick@yandex.ru>
2023-10-08 15:26:46 +0300

    updated some files

---
+++ app/controllers/recommendations_controller.rb
@@ -0,0 +1,36 @@
+class RecommendationsController < ApplicationController
+	before_action :get_consultation
+	before_action :get_patient, only: %i[show update destroy]
+
+	def index
+		@patients = Patient.all
+
+		render json: @patients
+	end
+
+	def show
+		render json: @patient
+	end
+
+	def create
+		@patient.create!(patient_params)
+	end
+
+	def update
+		@patient.update(patient_params)
+	end
+
+	def destroy
+		@patient.destroy
+	end
+
+	private
+
+	def patient_params
+		params.permit(:full_name, :birth_date, :phone, :email)
+	end
+
+	def get_patient
+		Patient.find(params[:patient_id])
+	end
+end
