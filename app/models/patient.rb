17b72b4a06f5acbdfe3ef1c15b4e3c05be101f5f
jingleville <andrewgavrick@yandex.ru>
2023-10-08 15:26:17 +0300

    added annotations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: patients
+#
+#  id         :bigint           not null, primary key
+#  birth_date :datetime
+#  email      :string
+#  full_name  :string
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Patient < ApplicationRecord
 	has_many :consultation_requests
 end
--- app/models/recomendation.rb
+++ app/models/recomendation.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: recomendations
+#
+#  id                      :bigint           not null, primary key
+#  recomendation_body      :text
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  consultation_request_id :bigint           not null
+#
+# Indexes
+#
+#  index_recomendations_on_consultation_request_id  (consultation_request_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (consultation_request_id => consultation_requests.id)
+#
 class Recomendation < ApplicationRecord
   belongs_to :consultation_request
 end
